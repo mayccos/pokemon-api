026ce767b77e2d5c4992a0071958280dd2029c7a
jean-christophe Becker <mothos@hotmail.fr>
2023-11-27 23:24:10 +0100

    fix commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(favicon(__dirname + '/favicon.ico'))
     .use(bodyParser.json())
     .use(cors())
 
-sequelize.initDb()
+//sequelize.initDb()
 
 app.get('/', (req, res) => {
     res.json('Hello, Heroku ! 👋')
